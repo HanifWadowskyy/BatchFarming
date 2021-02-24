e670a8f9739e5f940230e3514bfc2924c8e47ac2
PresentKim <debe3721@gmail.com>
2021-02-24 16:15:36 +0900

    Set license to LGPL and add PHP DOC

--- src/kim/present/batchfarming/entity/TargetingFallingEntity.php
+++ src/kim/present/batchfarming/entity/TargetingFallingEntity.php
@@ -1,4 +1,30 @@
-<?php /** @noinspection PhpInternalEntityUsedInspection */
+<?php
+
+/**
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  PresentKim (debe3721@gmail.com)
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ *
+ * @noinspection PhpIllegalPsrClassPathInspection
+ * @noinspection PhpDocSignatureInspection
+ * @noinspection SpellCheckingInspection
+ */
+
 declare(strict_types=1);
 
 namespace kim\present\batchfarming\entity;
@@ -16,6 +42,9 @@ use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\player\Player;
 use pocketmine\world\World;
+
+use function lcg_value;
+use function spl_object_hash;
 
 /**
  * Abstract class of entities falling into blocks of specified height.
